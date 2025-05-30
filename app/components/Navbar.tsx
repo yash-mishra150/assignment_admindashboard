3a1d2e19499b2486076043a1a253b27c233ddc53
Yash Mishra <praisedboy5000@gmail.com>
2025-05-31 01:13:46 +0530

    corrections

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import * as React from 'react';
+import { usePathname } from 'next/navigation'
 import Image from 'next/image';
 import logo from '@/public/logoplace.svg'
 
 const Navbar = () => {
+    const pathname = usePathname();
+
+    if(pathname == '/') return null; // Hide Navbar on the login page
+
     return (
         <div className="flex h-[10vh] justify-between px-[3vw] bg-[#083434] p-4 ">
             <Image src={logo} alt="Logo" className="w-28" />
